db2ca5accd77abacc9c34627c7941ecac6dbf0b8
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-18 16:42:28 +0900

    VideoUploadPage 라우팅

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
-
+import VideoUploadPage from './views/VideoUploadPage/VideoUploadPage'
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
@@ -21,6 +21,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path='/video/upload' component={Auth(VideoUploadPage, true)}/> {/** false : 로그인 한 사람은 안되게 , true : 로그인 한 사람만 되게*/}
         </Switch>
       </div>
       <Footer />
