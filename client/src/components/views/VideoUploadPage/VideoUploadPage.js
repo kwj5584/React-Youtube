f843baf91feb21313f64d1169cbc5e76afd15d59
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-19 15:42:34 +0900

    이미지 등록, 섬네일 구현

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon} from 'antd';
 import Dropzone from 'react-dropzone';
-
+import axios from 'axios'
 const {Title} = Typography;
 const {TextArea} = Input;
 
@@ -22,6 +22,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState('')
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState('')
+    const [Duration, setDuration] = useState('')
+    const [ThumbnailPath, setThumbnailPath] = useState('')
 
     const onTitleChange = (e) =>{
         setVideoTitle(e.currentTarget.value)
@@ -35,6 +38,39 @@ function VideoUploadPage() {
     const onCategoryChange = (e) =>{
         setCategory(e.currentTarget.value)
     }
+    const onDrop = (files) =>{
+        let formData = new FormData;
+        const config = {
+            header: {'content-type' : 'multipart/form-data'}
+        }
+        formData.append("file",files[0])
+
+        axios.post('/api/video/uploadfiles',formData, config)
+        .then(res=>{
+            if(res.data.success){
+
+                let variable = {
+                    filePath : res.data.filePath,
+                    fileName : res.data.fileName
+                }
+                setFilePath(res.data.filePath)
+
+                axios.post('/api/video/thumbnail', variable)
+                .then(res=>{
+                    if(res.data.success){
+                        // 정보를 state에 저장
+                        setDuration(res.data.fileDuration)
+                        setThumbnailPath(res.data.thumbsFilePath)
+
+                    }else{
+                        alert('썸네일 생성에 실패했습니다.')
+                    }
+                })
+            } else{
+                alert('비디오 업로드 실패')
+            }
+        })
+    }
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
             <div style={{ textAlign:'center', marginBottom:'2rem'}}>
@@ -44,9 +80,9 @@ function VideoUploadPage() {
                 <div style={{display:'flex', justifyContent: 'space-between'}}>
                     {/**Drop Zone */}
                         <Dropzone
-                        onDrop
-                        multiple
-                        maxSize
+                        onDrop={onDrop}
+                        multiple={false} // 한번에 파일 한개 올릴거면 false
+                        maxSize={10000000}
                         >
                         {({getRootProps, getInputProps}) => (
                             <div style={{width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
@@ -58,9 +94,11 @@ function VideoUploadPage() {
                         )}
                         </Dropzone>
                     {/**Tumbnail */}
-                    <div>
-                        <img src alt/>
-                    </div>
+                    { ThumbnailPath!=='' &&
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"/>
+                        </div>
+                    }
                 </div>
             <br/>
             <br/>
