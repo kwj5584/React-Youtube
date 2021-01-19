f33d015399640ca5eb05f90be2ba22e4a2a86c76
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-19 16:52:46 +0900

    videoUpload api 구현

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const { Video } = require("../models/Video");
+const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
 const multer = require('multer');
@@ -73,6 +73,15 @@ router.post('/thumbnail',(req,res)=>{
         size:'320x240',
         // %b : input basename( filename w/o extension)
         filename:'thumbnail-%b.png'
+    })
+})
+
+router.post('/uploadVideo',(req,res)=>{
+    // 비디오 정보들을 저장
+    const video = new Video(req.body)
+    video.save((err,doc)=>{
+        if(err) res.status.json({success:false, err})
+        res.status(200).json({success:true})
     })
 })
 
