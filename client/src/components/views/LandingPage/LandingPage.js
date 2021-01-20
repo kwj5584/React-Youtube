05e8224570464d14bf421c106055f85605f09068
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-20 13:49:18 +0900

    랜딩페이지에 비디오리스트 나타나게하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,58 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import {Card, Icon, Avatar, Col, Typography, Row} from 'antd';
+import axios from 'axios'
+import moment from 'moment';
+
+const {Title} = Typography
+const {Meta} = Card
 
 function LandingPage() {
+    const [Video, setVideo] = useState([])
+    useEffect(() => {
+        axios.get('/api/video/getVideos')
+        .then(res=>{
+            if(res.data.success){
+                setVideo(res.data.videos)
+            } else{
+                alert('비디오 가져오기 실패')
+            }
+        })
+    }, [])
+    const renderCards = Video.map((video,index)=>{
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor((video.duration- minutes*60));
+        return(
+            <Col lg={6} md={8} xs={24}> {/** 가장 큰 화면일 때 6*4=24 4개의 영상을 한 행에 등록, 중간크기일 때 3개, 가장 작은 크기일 때 1개 */}
+                <a href={`/video/post/${video._id}`}> 
+                    <div style={{position:'relative'}}>
+                        <img style={{width:'100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt='thumbnail'/>
+                        <div className="duration">
+                            <span>{minutes} : {seconds}</span>
+                        </div>
+                    </div>
+                    </a>
+                    <br/>
+                    <Meta
+                        avatar ={
+                            <Avatar src={video.writer.image}/> // 업로더 정보 
+                        }
+                        title={video.title}
+                        description=""
+                    />
+                    <span>{video.writer.name}</span><br/>
+                    <span style={{marginLeft:'3rem'}}>{video.views} views</span> - <span>{moment(video.createdAt).format('MMM Do YY')}</span> {/**업데이트 한 날짜 */}
+                </Col>
+        )
+    })
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{width:'85%', margin: '3rem auto'}}>
+            <Title level={2}>Recommended</Title>
+            <hr/>
+            <Row gutter={[32,16]}>
+                {renderCards}
+            </Row>
+        </div>
     )
 }
 
