ded58eec07ef5bb6abe817b33d81636cadbd874b
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-20 13:49:56 +0900

    getVideo 구현

--- server/routes/video.js
+++ server/routes/video.js
@@ -85,4 +85,14 @@ router.post('/uploadVideo',(req,res)=>{
     })
 })
 
+router.get('/getVideos',(req,res)=>{
+    // 비디오를 DB에서 가져와서 클라이언트에 보낸다.
+    Video.find()
+    .populate('writer')
+    .exec((err,videos)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).json({success:true, videos})
+    })
+})
+
 module.exports = router;
