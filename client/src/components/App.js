b99f58537ef5c1f4b61e649ff22e176a0cb916b2
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-20 15:31:40 +0900

    VideoDetailPage Routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 import VideoUploadPage from './views/VideoUploadPage/VideoUploadPage'
+import VideoDetailPage from './views/VideoDetailPage/VideoDetailPage'
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
@@ -22,6 +23,7 @@ function App() {
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path='/video/upload' component={Auth(VideoUploadPage, true)}/> {/** false : 로그인 한 사람은 안되게 , true : 로그인 한 사람만 되게*/}
+          <Route exact path="/video/:videoId" component={Auth(VideoDetailPage, null)} />
         </Switch>
       </div>
       <Footer />
