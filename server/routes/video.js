2ea77d1832d4d2aab17a6490e1f49765b813219a
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-20 15:33:51 +0900

    VideoDetailPage api 구현

--- server/routes/video.js
+++ server/routes/video.js
@@ -95,4 +95,14 @@ router.get('/getVideos',(req,res)=>{
     })
 })
 
+router.post('/getVideoDetail',(req,res)=>{
+    // 비디오를 DB에서 가져와서 클라이언트에 보낸다.
+    Video.findOne({ "_id": req.body.videoId})
+    .populate('writer')
+    .exec((err, videoDetail)=>{
+        if(err) return res.status(400).send(err)
+        res.status(200).json({ success:true, videoDetail })
+    })
+})
+
 module.exports = router;
