3b0151abac945190db31b4a684b51e0b9ddc5ae6
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-22 15:51:55 +0900

    Comment routing, getComment api, refreshFunction구현
    하위 컴포넌트에서 댓글 작성시 상위컴포넌트에서 가져올 수 있도록 구현

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,26 +3,41 @@ import {Row, Col, List, Avatar} from 'antd'
 import axios from 'axios'
 import SideVideo from './Sections/SideVideo'
 import Subscribe from './Sections/Subscribe'
+import Comment from './Sections/Comment'
+
 
 function VideoDetailPage(props) {
     
     const videoId = props.match.params.videoId
     const [VideoDetail, setVideoDetail] = useState([])
-
+    const [Comments, setComments] = useState([])
     const variable = { videoId:videoId }
     
     useEffect(() => {
         axios.post('/api/video/getVideoDetail',variable)        
         .then(res=>{
             if(res.data.success){
-                console.log(res.data)
                 setVideoDetail(res.data.videoDetail)
             }else{
                 alert('비디오 정보 가져오기 실패')
             }
         })
+        axios.post('/api/comment/getComments', variable)
+        .then(res=>{
+            if(res.data.success){
+                setComments(res.data.comments)
+            }else{
+                alert('댓글 정보 가져오는 것을 실패')
+            }
+        })
     }, [])
+
+    const refreshFunction = (newComment) =>{
+        setComments(Comments.concat(newComment))
+    }
     if(VideoDetail.writer){
+
+        const subscribeButton = VideoDetail.writer._id !== localStorage.getItem('userId') &&  <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>
         return (
             <Row gutter={[16,16]}>
                 <Col lg={18} xs={24}>
@@ -30,7 +45,7 @@ function VideoDetailPage(props) {
                         <video style={{width:'100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls/>
     
                         <List.Item
-                            actions={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>]}
+                            actions={[subscribeButton]}
                         >
                             <List.Item.Meta
                                 avatar={<Avatar src={VideoDetail.writer.image}/>}
@@ -40,6 +55,7 @@ function VideoDetailPage(props) {
                         </List.Item>
     
                         {/* Comments*/}
+                        <Comment refreshFunction={refreshFunction} commentLists={Comments} videoId={videoId}/>
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
