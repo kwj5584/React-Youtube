4f0374504462ef45c9f561577ddd68e0dab5f5a0
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-23 16:24:21 +0900

    SingleComment (오류 미수정)

--- client/src/components/views/VideoDetailPage/Sections/SingleComment.js
+++ client/src/components/views/VideoDetailPage/Sections/SingleComment.js
@@ -1,65 +1,71 @@
-import React,{useState} from 'react'
-import { Comment, Avatar, Button, Input} from 'antd';
-import axios from 'axios'
+import React, { useState } from 'react'
+import { Comment, Avatar, Button, Input } from 'antd';
+import Axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const {TextArea} = Input;
-
+const { TextArea } = Input;
 function SingleComment(props) {
-
-    const postId = props.postId
-    const writer = props.writer
+    console.log('single:',props)
+    const user = useSelector(state => state.user);
+    const [CommentValue, setCommentValue] = useState("")
     const [OpenReply, setOpenReply] = useState(false)
-    const [CommentValue, setCommentValue] = useState('')
 
-    const onClickReplyOpen = () =>{
-        setOpenReply(!OpenReply)
-    }
-    const onHandleChange = (e) =>{
+    const handleChange = (e) => {
         setCommentValue(e.currentTarget.value)
     }
-    const onSubmit = (e) =>{
+
+    const openReply = () => {
+        setOpenReply(!OpenReply)
+    }
+
+    const onSubmit = (e) => {
         e.preventDefault();
 
-        const commentVariable = {
-            content: CommentValue,
-            writer: writer,
-            postId: postId,
-            responseTo : props.comment._id
+        const variables = {
+            writer: user.userData._id,
+            postId: props.postId,
+            responseTo: props.comment._id,
+            content: CommentValue
         }
-        axios.post('/api/comment/saveComment', commentVariable)
-        .then(res=>{
-            if(res.data.success){
-                props.refreshFunction(res.data.result)
-                setCommentValue('')
-            }else{
-                alert('댓글 작성 실패')
-            }
-        })
+
+        Axios.post('/api/comment/saveComment', variables)
+            .then(res => {
+                if (res.data.success) {
+                    setCommentValue("")
+                    setOpenReply(!OpenReply)
+                    props.refreshFunction(res.data.result)
+                } else {
+                    alert('Failed to save Comment')
+                }
+            })
     }
+
     const actions = [
-        <span onClick={onClickReplyOpen} key='comment-basic-reply-to'>Reply to</span>
+        <span onClick={openReply} key="comment-basic-reply-to">Reply to </span>
     ]
+
     return (
         <div>
             <Comment
                 actions={actions}
                 author={props.comment.writer.name}
-                avatar={<Avatar src={props.comment.writer.image} alt/>}
+                avatar={<Avatar src={props.comment.writer.image} alt='image'/>}
                 content={ <p>{props.comment.content}</p>}
             />
+
             {OpenReply &&
-                <form style={{display:'flex'}} onSubmit={onSubmit}>
-                <textarea
-                    style={{width:'100%', borderRadius:'5px'}}
-                    onChange={onHandleChange}
-                    value={CommentValue}
-                    placeholder='댓글 작성해주세요'
-                />
-                <br/>
-                <button style={{width:'20%', height:'52px'}} onClick={onSubmit}>Submit</button>
-            </form>
+                <form style={{ display: 'flex' }} onSubmit={onSubmit}>
+                    <TextArea
+                        style={{ width: '100%', borderRadius: '5px' }}
+                        onChange={handleChange}
+                        value={CommentValue}
+                        placeholder="write some comments"
+                    />
+                    <br />
+                    <Button style={{ width: '20%', height: '52px' }} onClick={onSubmit}>Submit</Button>
+                </form>
             }
-            
+
         </div>
     )
 }
