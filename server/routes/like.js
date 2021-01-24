8a4ad3bde79558bdb3b595b22656eaaf25f22c32
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-24 17:40:46 +0900

    좋아요,싫어요 api 구현

--- server/routes/like.js
+++ server/routes/like.js
@@ -25,9 +25,9 @@ router.post('/getLikes',(req,res)=>{
 router.post('/getDislikes',(req,res)=>{
     let variable = {}
     if(req.body.videoId) {
-        variable = { videoId: req.body.videoId}
+        variable = { videoId: req.body.videoId, userId: req.body.userId}
     } else{
-        variable = {commentId: req.body.commentId}
+        variable = {commentId: req.body.commentId, userId: req.body.userId}
     }
     Dislike.find(variable)
     .exec((err,dislikes)=>{
@@ -35,6 +35,75 @@ router.post('/getDislikes',(req,res)=>{
         res.status(200).json({success: true, dislikes})
     })
 })
+
+router.post('/upLike',(req,res)=>{
+    let variable = {}
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId}
+    } else{
+        variable = {commentId: req.body.commentId, userId: req.body.userId}
+    }
+
+    // Like Collection에 정보를 저장
+    const like = new Like(variable)
+    like.save((err, likeResult)=>{
+        if(err) return res.status(400).send(err)
+    // 만약 싫어요가 이미 클릭이 되있다면 싫어요값 1 줄여줌
+        Dislike.findOneAndDelete(variable)
+            .exec((err,disLikeResult)=>{
+                if(err) return res.status(400).json({success:false, err})
+                res.status(200).json({success: true})
+            })
+    });
+})
+router.post('/unLike',(req,res)=>{
+    let variable = {}
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId}
+    } else{
+        variable = {commentId: req.body.commentId}
+    }
+    Like.findOneAndDelete(variable)
+    .exec((err,result) =>{
+        if(err) return res.status(400).json({success:false, err})
+        res.status(200).json({success: true})
+    })
+})
+
+router.post('/unDislike',(req,res)=>{
+    let variable = {}
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId}
+    } else{
+        variable = {commentId: req.body.commentId, userId: req.body.userId}
+    }
+    Dislike.findOneAndDelete(variable)
+    .exec((err,result) =>{
+        if(err) return res.status(400).json({success:false, err})
+        res.status(200).json({success: true})
+    })
+})
+
+router.post('/upDisLike',(req,res)=>{
+    let variable = {}
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId}
+    } else{
+        variable = {commentId: req.body.commentId, userId: req.body.userId}
+    }
+
+    // Like Collection에 정보를 저장
+    const dislike = new Dislike(variable)
+    dislike.save((err, dislikeResult)=>{
+        if(err) return res.status(400).send(err)
+    // 만약 좋아요가 이미 클릭이 되있다면 좋아요값 1 줄여줌
+        Like.findOneAndDelete(variable)
+            .exec((err,likeResult)=>{
+                if(err) return res.status(400).json({success:false, err})
+                res.status(200).json({success: true})
+            })
+    })
+    })
 
 
 module.exports = router;
