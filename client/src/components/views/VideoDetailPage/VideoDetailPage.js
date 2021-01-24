8a02892f62b6dfa5f28c58728c6dbd7138136af5
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-24 16:40:26 +0900

    LikeDislike 컴포넌트 추가

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import SideVideo from './Sections/SideVideo'
 import Subscribe from './Sections/Subscribe'
 import Comment from './Sections/Comment'
-
+import LikeDislikes from './Sections/LikeDislikes'
 
 function VideoDetailPage(props) {
     
@@ -46,7 +46,7 @@ function VideoDetailPage(props) {
                         <video style={{width:'100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls/>
     
                         <List.Item
-                            actions={[subscribeButton]}
+                            actions={[ <LikeDislikes video userId={localStorage.getItem('userId')} videoId={videoId} />, subscribeButton]}
                         >
                             <List.Item.Meta
                                 avatar={<Avatar src={VideoDetail.writer && VideoDetail.writer.image}/>}
