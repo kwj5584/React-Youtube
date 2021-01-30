40fc25e999ccba8aea41befb8dc2d85ebdc4b68d
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-30 19:05:10 +0900

    Update LandingPage.js

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -22,7 +22,7 @@ function LandingPage() {
         var minutes = Math.floor(video.duration / 60);
         var seconds = Math.floor((video.duration- minutes*60));
         return(
-            <Col lg={6} md={8} xs={24}> {/** 가장 큰 화면일 때 6*4=24 4개의 영상을 한 행에 등록, 중간크기일 때 3개, 가장 작은 크기일 때 1개 */}
+            <Col key={index} lg={6} md={8} xs={24}> {/** 가장 큰 화면일 때 6*4=24 4개의 영상을 한 행에 등록, 중간크기일 때 3개, 가장 작은 크기일 때 1개 */}
                 <div style={{position:'relative'}}>
                     <a href={`/video/${video._id}`}> 
                         <img style={{width:'100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt='thumbnail'/>
@@ -37,10 +37,9 @@ function LandingPage() {
                             <Avatar src={video.writer.image}/> // 업로더 정보 
                         }
                         title={video.title}
-                        description=""
                     />
                     <span>{video.writer.name}</span><br/>
-                    <span style={{marginLeft:'3rem'}}>{video.views} views</span>
+                    <span style={{marginLeft:'3rem'}}>조회수 {video.views} 회</span>
                      - <span>{moment(video.createdAt).format('MMM Do YY')}</span> {/**업데이트 한 날짜 */}
                 </Col>
         )
