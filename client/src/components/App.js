35f904f79d79fcf90acab225c7879712e3b9ff09
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-31 17:19:07 +0900

    SearchPage Routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,6 +10,7 @@ import Footer from "./views/Footer/Footer"
 import VideoUploadPage from './views/VideoUploadPage/VideoUploadPage'
 import VideoDetailPage from './views/VideoDetailPage/VideoDetailPage'
 import SubscriptionPage from './views/SubscriptionPage/SubscriptionPage'
+import SearchPage from './views/SearchPage/SearchPage'
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -21,12 +22,14 @@ function App() {
       <NavBar />
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
+          {/* <LandingPage/> */}
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path='/video/upload' component={Auth(VideoUploadPage, true)}/> {/** false : 로그인 한 사람은 안되게 , true : 로그인 한 사람만 되게*/}
           <Route exact path="/video/:videoId" component={Auth(VideoDetailPage, null)} />
           <Route exact path="/subscription" component={Auth(SubscriptionPage, null)} />
+          <Route exact path="/result" component={Auth(SearchPage, null)}/>
         </Switch>
       </div>
       <Footer />
