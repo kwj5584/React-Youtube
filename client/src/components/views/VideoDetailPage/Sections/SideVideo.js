016d78197f14a44dce5058adf20c7d99f8a97b0a
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-02-02 14:52:47 +0900

    Update SideVideo.js

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -29,7 +29,7 @@ function SideVideo() {
                     <a href={`/video/${video._id}`} style={{color:'gray '}}>
                         <span style={{fontSize:'1rem',color:'black'}}>{video.title}</span><br/>
                         <span>{video.writer.name}</span><br/>
-                        <span>{video.views}</span><br/>
+                        <span>조회수 {video.views}</span><br/>
                         <span>{minutes} : {seconds}</span>
                     </a>
                 </div>
