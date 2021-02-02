b6ab6d4b00b065920b6bdf65afd5d633afc8f0eb
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-02-02 16:49:29 +0900

    우측 상단 아바타 드랍다운형식으로 변경

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import { Menu } from 'antd';
+import React,{useState} from 'react';
+import { Menu, Avatar, Dropdown} from 'antd';
 import axios from 'axios';
 import { USER_SERVER } from '../../../Config';
 import { withRouter } from 'react-router-dom';
 import { useSelector } from "react-redux";
+import {UserOutlined} from '@ant-design/icons'
 
 function RightMenu(props) {
   const user = useSelector(state => state.user)
-
   const logoutHandler = () => {
     axios.get(`${USER_SERVER}/logout`).then(response => {
       if (response.status === 200) {
@@ -18,10 +18,23 @@ function RightMenu(props) {
       }
     });
   };
-
-  if (user.userData && !user.userData.isAuth) {
-    return (
-      <Menu mode={props.mode}>
+  const onDropdown = (e) =>{
+    e.preventDefault();
+    
+  }
+  const loginUserMenu = (
+    <Menu >
+    <Menu.Item key='upload'>
+      <a href='/video/upload'>Video</a>
+    </Menu.Item>
+    
+    <Menu.Item key="logout">
+      <a onClick={logoutHandler}>Logout</a>
+    </Menu.Item>
+  </Menu>
+  )
+  const notLoginUserMenu = (
+    <Menu>
         <Menu.Item key="mail">
           <a href="/login">Signin</a>
         </Menu.Item>
@@ -29,17 +42,21 @@ function RightMenu(props) {
           <a href="/register">Signup</a>
         </Menu.Item>
       </Menu>
+  )
+  if (user.userData && !user.userData.isAuth) {
+    return (
+      <Dropdown overlay={notLoginUserMenu}>
+        <Avatar style={{marginTop:'13px'}} size='large' icon={<UserOutlined />} />
+      </Dropdown>
     )
   } else {
     return (
-      <Menu mode={props.mode}>
-        <Menu.Item key='upload'>
-          <a href='/video/upload'>Video</a>
-        </Menu.Item>
-        <Menu.Item key="logout">
-          <a onClick={logoutHandler}>Logout</a>
-        </Menu.Item>
-      </Menu>
+      <div>
+        { <Dropdown overlay={loginUserMenu}>
+        <Avatar style={{marginTop:'13px'}} size='large' src={user.userData && user.userData.image} onClick={onDropdown}/>
+        </Dropdown>
+        }
+      </div>
     )
   }
 }
