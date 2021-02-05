d36db8f435622cceca3fac75a7a23794b71abe8e
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-02-06 03:49:37 +0900

    Update SearchPage.js

--- client/src/components/views/SearchPage/SearchPage.js
+++ client/src/components/views/SearchPage/SearchPage.js
@@ -7,7 +7,6 @@ const {Meta} = Card
 
 
 function SearchPage(props) {
-    console.log('searchPage :',props.location.state.searchResult)
     const [FindVideo, setFindVideo] = useState([])
     useEffect(() => {
         let variable = {search : props.location.state.searchResult}
